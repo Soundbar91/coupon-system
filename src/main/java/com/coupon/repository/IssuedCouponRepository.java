b6acf36ac01f68e83961a2e17fcae4d8f0d99e89
신관규 <soundbar91@gmail.com>
2026-05-12 17:03:24 +0900

    쿠폰 발급 이력 조회 메소드 네이밍 수정

--- src/main/java/com/coupon/repository/IssuedCouponRepository.java
+++ src/main/java/com/coupon/repository/IssuedCouponRepository.java
@@ -1,14 +1,12 @@
 package com.coupon.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.Repository;
 
 import com.coupon.entity.IssuedCoupon;
 
 public interface IssuedCouponRepository extends Repository<IssuedCoupon, Long> {
 
-    Optional<IssuedCoupon> findByCouponIdAndUserId(Long couponId, Long userId);
+    boolean existByCouponIdAndUserId(Long couponId, Long userId);
 
     Integer countByCouponId(Long couponId);
 
